aaf4f2ee2f6aed0347dd5dceda3329f58fdc9c0c
gux928 <gux928@qq.com>
2025-06-03 10:46:10 +0800

    Update rendezvous server address and public key in config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.79.89.16"];
+pub const RS_PUB_KEY: &str = "Q+KAIsMy9GZ4q+Rj2UAnIM0frC21XMnjsbeV41COIic=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
